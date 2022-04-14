2e8dfb6a56fe1fc372ba3ecc422fd6256f4f882a
Peter <peter.zgurovets@gmail.com>
2022-04-13 23:25:55 -0700

    Add files via upload

--- DOM - Event/index.js
+++ DOM - Event/index.js
@@ -1,11 +1,25 @@
-//Selects Input tag.
+const paraGoogle = document.createElement("p");
+const nodeGoogle = document.createTextNode("This is new.");
+paraGoogle.appendChild(nodeGoogle);
+
+
+const paraBing = document.createElement("p");
+const nodeBing = document.createTextNode("This is new.");
+paraBing.appendChild(nodeBing);
+
+const paraDuckDuckGo = document.createElement("p");
+const nodeDuckDuckGo = document.createTextNode("This is new.");
+paraDuckDuckGo.appendChild(nodeDuckDuckGo);
+
+
+
 var inputVisibility = document.querySelector("input");
 // Selects Button tag.
 var deleteLink = document.querySelector('button');
-//Stores the search engine buttons tag.
-var searchButtons = document.querySelectorAll('.search');
-var searchBing = document.querySelector(".search_Bing");
-var searchDuckDuckGo = document.querySelector(".search_DuckDuckGo");
+//Stores the search engine button class names.
+var searchButtons = document.querySelectorAll('.search_engines button');
+
+var element = document.querySelectorAll('.search_engines li');
 
 //The function listen to the input tag and assigns or sunsets a hidden attribute to a button.
 inputVisibility.addEventListener('click', function(event) {
@@ -16,7 +30,6 @@ inputVisibility.addEventListener('click', function(event) {
   } else {
     return deleteLink.hidden = false;
   }
-
 });
 
 //Functon listent to button tag.
@@ -31,5 +44,38 @@ deleteLink.addEventListener('click', function(event) {
   }
 });
 
+//Search engine buttom tags listener. ADD and DELETE HTML tag <p>.
 
-//Search engine buttoms listener.
+Array.from(searchButtons).forEach(link => {
+  link.addEventListener('click', function(event) {
+    var tagName;
+
+    if ("node" + event.target.className === "nodeGoogle") {
+
+      if (document.body.contains(document.querySelector(".paraGoogle")) == false) {
+        const buttonGoogle = element[0].appendChild(paraGoogle).classList.add("paraGoogle");
+      } else {
+        const className = document.querySelectorAll('.search_engines .search_G p');
+        className[0].remove();
+      }
+
+    } else if ("node" + event.target.className === "nodeBing") {
+
+      if (document.body.contains(document.querySelector(".paraBing")) == false) {
+        const buttonBing = element[1].appendChild(paraBing).classList.add("paraBing");
+      } else {
+        const className = document.querySelectorAll('.search_engines .search_B p');
+        className[0].remove();
+      }
+
+    } else if ("node" + event.target.className === "nodeDuckDuckGo") {
+
+      if (document.body.contains(document.querySelector(".paraDuckDuckGo")) == false) {
+        const buttonDuckDuckGo = element[2].appendChild(paraDuckDuckGo).classList.add("paraDuckDuckGo");
+      } else {
+        const className = document.querySelectorAll('.search_engines .search_D p');
+        className[0].remove();
+      }
+    }
+  });
+});

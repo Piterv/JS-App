6fe0b97793a85c0cbf6a0cbda2dd45b60287f7ca
Peter <peter.zgurovets@gmail.com>
2022-04-22 13:38:46 -0700

    Add files via upload
    Code revising: for the keydown event, the switch statement has been changed to if.

--- Calculator/index.js
+++ Calculator/index.js
@@ -10,62 +10,9 @@ para.forEach(item => {
 });
 
 document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case '0':
-      console.log(event.key);
-      break;
-    case '1':
-      console.log(event.key);
-      break;
-    case '2':
-      console.log(event.key);
-      break;
-    case '3':
-      console.log(event.key);
-      break;
-    case '4':
-      console.log(event.key);
-      break;
-    case '5':
-      console.log(event.key);
-      break;
-    case '6':
-      console.log(event.key);
-      break;
-    case '7':
-      console.log(event.key);
-      break;
-    case '8':
-      console.log(event.key);
-      break;
-    case '9':
-      console.log(event.key);
-      break;
-    case '/':
-      console.log(event.key);
-      break;
-    case '-':
-      console.log(event.key);
-      break;
-    case '+':
-      console.log(event.key);
-      break;
-    case '=':
-      console.log(event.key);
-      break;
-    case '%':
-      console.log(event.key);
-      break;
-    case '(':
-      console.log(event.key);
-      break;
-    case ')':
-      console.log(event.key);
-      break;
-      case 'x':
-        console.log(event.key);
-        break;
-    default:
-      console.log('false');
+  const num = event.key;
+  if (num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9' || num == '-' || num == '+' || num == '/' || num == '=' || num == 'CE' || num == '.' || num == '(' || num == ')' || num == '%' || num == 'x') {
+    console.log(event.key);
+    
   }
 });

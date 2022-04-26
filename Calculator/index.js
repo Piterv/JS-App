e05c0917c9e1e0b9fb98db8033a9ee64e8a08678
Peter <peter.zgurovets@gmail.com>
2022-04-26 12:12:04 -0700

    Add files via upload
    Code refactoring.

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,89 +1,61 @@
-const para = document.querySelectorAll('.digit');
+const symbol = document.querySelectorAll('.digit');
 const calInput = document.getElementById('calculator-input');
-
+const defaultInput = document.getElementById('calculator-input').textContent = "0";
 //Stores typed numbers
-var firstNumbers = [];
-var secondNumbers = [];
+let firstNumbers = [];
+let secondNumbers = [];
 // Stores sign + - or / *
-var arithmeticSign = [];
-// Set all to the begining
-var reset;
-// Stores equation result.
-let result;
-//
-
+let arithmeticSign = [];
 //Calls on mouse click
-const mouseClick = para.forEach(item => {
+const mouseClick = symbol.forEach(item => {
   item.addEventListener('click', event => {
+
     const num = event.target.innerText;
-    if (num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9' || num == '.') {
-      if (arithmeticSign.length === 0) {
-        firstNumbers.push(event.target.innerText);
-        calInput.textContent = parseInt(firstNumbers.join(''));
-        console.log(firstNumbers);
-      } else if (arithmeticSign.length === 1) {
-        secondNumbers.push(event.target.innerText);
-        calInput.append(textContent = parseInt(secondNumbers.join('')));
-        console.log(secondNumbers);
-      }
-    } else if (num == '-' || num == '+' || num == '/' || num == '*') {
-      if (arithmeticSign.length === 0) {
-        arithmeticSign.push(num);
-        calInput.append(textContent = arithmeticSign[0]);
-        console.log(arithmeticSign);
-      } else if (arithmeticSign.length === 1) {
-        arithmeticSign.pop();
-        arithmeticSign.push(num);
-        calInput.append(textContent = arithmeticSign[0]);
-        console.log(arithmeticSign);
-      }
-    } else if (num == 'CE') {
-      console.log(reset);
-    } else if (num == '=') {
-      result = calculation();
-      calInput.textContent = result;
-      console.log(result);
-    } else if (num == '(' || num == ')') {
-      firstNumbers.push(event.target.innerText);
-      console.log(chusenSign);
-    }
+    calculator(num);
   });
 });
 //Calls on keydown.
-document.addEventListener('keydown', (event) => {
+const keydownPress = document.addEventListener('keydown', (event) => {
+
   const num = event.key;
-
-  if (num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9' || num == '.') {
-    if (arithmeticSign.length === 0) {
-      firstNumbers.push(event.key);
-      console.log(firstNumbers);
-    } else if (arithmeticSign.length === 1) {
-      secondNumbers.push(event.key);
-      console.log(secondNumbers);
-    }
-  } else if (num == '-' || num == '+' || num == '/' || num == '*') {
-    if (arithmeticSign.length === 0) {
-      arithmeticSign.push(num);
-      console.log(arithmeticSign);
-    } else if (arithmeticSign.length === 1) {
-      arithmeticSign.pop();
-      arithmeticSign.push(num);
-      console.log(arithmeticSign);
-    }
-  } else if (num == 'CE') {
-    console.log(reset);
-
-  } else if (num === '=') {
-    result = calculation();
-    calInput.textContent = result;
-    console.log(result);
-  } else if (num == '(' || num == ')') {
-    firstNumbers.push(event.key);
-    console.log(chusenSign);
-  }
+  calculator(num);
 });
-
-
+// Function manage calculator.
+function calculator(num) {
+  if (num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9' || num == '.') {
+    setNumber(num);
+  } else if (num == '-' || num == '+' || num == '/' || num == '*') {
+    setArithmeticSign(num);
+  } else if (num == 'CE') {
+    reset();
+  } else if (num === '=') {
+    calculation();
+    calInput.textContent = calculation();
+  } else if (num == '(' || num == ')') {
+    firstNumbers.push(num);
+  }
+}
+//Set number.
+function setNumber(num) {
+  if (arithmeticSign.length === 0 && firstNumbers.length <= 7) { //Accepts only 8 digits on the first set of numbers.
+    firstNumbers.push(num);
+    calInput.append(num);
+  } else if (arithmeticSign.length === 1 && secondNumbers.length <= 7) {
+    secondNumbers.push(num);
+    calInput.append(textContent = num);
+  }
+}
+// Set arithmetic sign.
+function setArithmeticSign(num) {
+  if (arithmeticSign.length === 0) {
+    arithmeticSign.push(num);
+    calInput.append(textContent = arithmeticSign[0]);
+  } else if (arithmeticSign.length === 1) {
+    arithmeticSign.pop();
+    arithmeticSign.push(num);
+    calInput.append(textContent = arithmeticSign[0]);
+  }
+}
 //Function calculation.
 function calculation() {
   let firstSetNumbers = parseInt(firstNumbers.join('')); //Convert array to integer numbers.
@@ -98,4 +70,11 @@ function calculation() {
   } else if (arithmeticSign[0] === '*') {
     return firstSetNumbers * secondSetNumbers;
   }
+}
+//Clean all variables, and set 0.
+function reset() {
+  calInput.textContent = defaultInput;
+  firstNumbers = [];
+  secondNumbers = [];
+  arithmeticSign = [];
 }

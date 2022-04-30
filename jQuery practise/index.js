fdff6bd1822bff1b1c2a358d81f31db1ca362969
Peter <peter.zgurovets@gmail.com>
2022-04-29 20:38:29 -0700

    Add files via upload

--- jQuery practise/index.js
+++ jQuery practise/index.js
@@ -1,1 +1,21 @@
-$('h1').css('color','red');
+
+//jQuery
+$('button').click(function(){
+  $("h1").css("color", "purple");
+  console.log($('h1').attr('style'));
+});
+
+
+//
+// //Pure JavaScript
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll('button')[i].addEventListener("click", function(){
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+// // Another JS. way.
+// document.querySelectorAll("button").forEach((button) => {
+//   button.addEventListener("click", _=>{
+//    document.querySelector("h1").style.color = "purple";
+//   })
+// });

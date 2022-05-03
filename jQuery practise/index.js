ea546167cabd0b38b592864a7237067843337b92
Peter <peter.zgurovets@gmail.com>
2022-05-03 11:37:34 -0700

    Add files via upload

--- jQuery practise/index.js
+++ jQuery practise/index.js
@@ -1,11 +1,28 @@
-
 //jQuery
-$('button').click(function(){
-  $("h1").css("color", "purple");
-  console.log($('h1').attr('style'));
+$('button').on('click', (e)=>{
+
+  console.log(e.target['name']);
+
+  if (e.target['name'] === 'buttonSlide') {
+    $('h1').slideToggle();
+  }else{
+    $('h1').toggleClass('heading', 'addOrRemove');
+  }
 });
 
-
+//
+// // jQuery
+// $('button').click(function() {
+//
+//   if ($("h1").attr('style') === 'color: purple;') {
+//     $("h1").removeAttr("style");
+//   } else {
+//     $("h1").css("color", "purple");
+//   }
+// });
+//
+//
+//
 //
 // //Pure JavaScript
 // for (var i = 0; i < 5; i++) {

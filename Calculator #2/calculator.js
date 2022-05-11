733f1166e15cc99f8ea04a28ef2ad46a1d1e7075
Peter <peter.zgurovets@gmail.com>
2022-05-11 09:23:20 -0700

    Calculator app.
    Simple calculator app using Node, Express.

---
+++ Calculator #2/calculator.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', (req, res) => {
+
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+
+  let result = num1 + num2;
+  res.send('The result is ' + result);
+});
+// spean up the server.
+app.listen(3000, () => {
+  console.log("Port 3000");
+});

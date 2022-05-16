a62850e367afaefa352c8c94c15d4fc1d1719061
Peter <peter.zgurovets@gmail.com>
2022-05-16 08:46:24 -0700

    Add files via upload

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -10,23 +10,30 @@ app.use(bodyParser.urlencoded({
 
 // Respond on request with loading Home page.
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
 
   //URL to get information from.
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=250b9be7c051920031529c06569a4ced&units=metric&lang=ua';
- //HTTPS get request to openweathermap.org
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=250b9be7c051920031529c06569a4ced&units=metric';
+  //HTTPS get request to openweathermap.org
   https.get(url, (response) => {
-    console.log(response.statusCode);
-    response.on('data', (data) => {
-      const weatherData = JSON.parse(data);
 
+    console.log(response.statusCode);
+
+    response.on('data', (data) => {
+      //Parse the received data into Json format.
+      const weatherData = JSON.parse(data);
+      //Store received data.
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.main.icon;
+      const weatherLocation = weatherData.name;
+      const weatherIcon = weatherData.weather[0].icon + '.png';
+      const icon = 'http://openweathermap.org/img/wn/'+ weatherIcon + '"';
 
-      console.log(temp);
-      console.log(weatherDescription );
-      console.log(weatherData);
+
+      //Send the received data to the user.
+      res.write('<p>The weather in ' + weatherLocation + ' is curently ' + weatherDescription + '</p>');
+      res.write('<h1> The temperature in London is ' + temp + ' degrees Celcius.</h1>');
+      res.write('<img src="http://openweathermap.org/img/wn/'+ weatherIcon + '" alt="weather img">');
+      res.send();
     })
   });
 
